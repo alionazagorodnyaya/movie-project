a468b1e36eb55921bf09bee0cad1588d91bf3683
alionazagorodnyaya <alenautub@icloud.com>
2026-05-21 23:10:12 +0400

    initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body>
+				<header></header>
+				<main>{children}</main>
+				<footer></footer>
+			</body>
+		</html>
+	);
+}
